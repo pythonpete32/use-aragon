454e3c702253721f4001f45e750831adc6aec122
pythonpete32 <aaron@blackscale.io>
2023-02-25 22:43:12 +0400

    feat: test hooks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,26 +2,51 @@ import { ConnectKitButton } from "connectkit";
 import { useAccount } from "wagmi";
 import { Account } from "./components";
 
-import { useFetchDaos } from "../../src";
+import {
+  useDepositEth,
+  useEstimateDepositEth,
+  useFetchDao,
+  useFetchDaoBalances,
+  useFetchDaos,
+  useFetchTransfers,
+} from "../../src";
+import { DepositParams, TokenType } from "@aragon/sdk-client";
+import { DepositEthParams } from "@aragon/sdk-client/dist/interfaces";
+
+const daoAddress = "0xa65a1e3181eb4c568121e323f888cb702f3944ba";
+const depositParams: DepositEthParams = {
+  type: TokenType.NATIVE,
+  daoAddressOrEns: daoAddress,
+  amount: BigInt(10), // amount in wei
+};
 
 export function App(): JSX.Element {
   const { isConnected } = useAccount();
-  useFetchDaos(
-    {},
-    {
-      onError(err) {
-        console.log({ "❌ useFetchDaos()": err });
-      },
-      onSuccess(data) {
-        console.log({ "✅ useFetchDaos()": data });
-      },
+  useFetchDaos();
+  useFetchDao(daoAddress);
+  useFetchTransfers({ daoAddressOrEns: daoAddress });
+  useFetchDaoBalances({ daoAddressOrEns: daoAddress });
+  useEstimateDepositEth(depositParams);
+  const { depositEth, isIdle, isLoading, isError } = useDepositEth(depositParams, {
+    onSuccess(data) {
+      console.log({ success: data });
     },
-  );
+  });
+
+  const handleDepositEth = async () => {
+    if (!depositEth) return;
+    depositEth(depositParams);
+  };
+
   return (
     <>
       <h1>wagmi + ConnectKit + Vite</h1>
       <ConnectKitButton />
       {isConnected && <Account />}
+      {isLoading && <p>loading...</p>}
+      {isError && <p>error</p>}
+      {isIdle && <p>idle</p>}
+      <button onClick={() => handleDepositEth()}>Deposit ETH</button>
     </>
   );
 }
