b1b03c0f324d24c69226db873cc1eaa650f136fc
pythonpete32 <aaron@blackscale.io>
2023-02-25 22:41:14 +0400

    feat: update paths

--- src/hooks/base/index.ts
+++ src/hooks/base/index.ts
@@ -2,3 +2,5 @@ export * from "./useFetchDao";
 export * from "./useFetchDaos";
 export * from "./useFetchDaoBalances";
 export * from "./useFetchTransfers";
+export * from "./useEstimateDepositEth";
+export * from "./useDepositEth";
