3409e3df56e171c1318f4a0a9b6e2b4a37d57573
pythonpete32 <aaron@blackscale.io>
2023-02-25 18:54:51 +0400

    feat: update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { DaoListItem, DaoDetails } from "@aragon/sdk-client";
+import { DaoListItem, DaoDetails, AssetBalance, Transfer } from "@aragon/sdk-client";
 import { UseQueryResult } from "react-query";
 
 export interface FetchDaosResult extends Omit<UseQueryResult<DaoListItem[], unknown>, "data"> {
@@ -8,3 +8,11 @@ export interface FetchDaosResult extends Omit<UseQueryResult<DaoListItem[], unkn
 export interface FetchDaoResult extends Omit<UseQueryResult<DaoDetails | null, unknown>, "data"> {
   dao: DaoDetails | null;
 }
+
+export type FetchDaoBalancesResult = UseQueryResult<AssetBalance[] | null, unknown> & {
+  daoBalances: AssetBalance[] | null;
+};
+
+export type FetchTransfersResult = UseQueryResult<Transfer[] | null, unknown> & {
+  transfers: Transfer[] | null;
+};
