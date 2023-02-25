81939393c21891342a2705cb0f218e451f4244f6
pythonpete32 <aaron@blackscale.io>
2023-02-25 22:42:24 +0400

    fix: only enable goerli

--- example/src/wagmi.ts
+++ example/src/wagmi.ts
@@ -1,10 +1,12 @@
 import { getDefaultClient } from "connectkit";
 import { createClient } from "wagmi";
+import { goerli } from "wagmi/chains";
 
 export const client = createClient(
   getDefaultClient({
     autoConnect: true,
     appName: "My wagmi + ConnectKit App",
     alchemyId: import.meta.env.VITE_ALCHEMY_API_KEY!,
+    chains: [goerli],
   }),
 );
