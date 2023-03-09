a7d3d8010302c94c5268a9ef9795896089e06b49
pythonpete32 <aaron@blackscale.io>
2023-03-09 16:16:34 +0400

    fixed `useDepositEth`

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,4 +4,4 @@ export * from './useFetchTransfers';
 export * from './useFetchDaoBalances';
 export * from './useEstimateDeposit';
 export * from './useDepositEth';
-// export * from './useDepositERC20';
+export * from './useDepositERC20';
