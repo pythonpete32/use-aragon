4c5eaf9315c2ba4b4e9355bf370ea6f61a39caa4
pythonpete32 <aaron@blackscale.io>
2023-03-10 17:22:27 +0400

    fix status bug and make easier to read

--- src/core/useNewDao/useNewDao.tsx
+++ src/core/useNewDao/useNewDao.tsx
@@ -1,48 +1,45 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 import { useMutation, UseMutationOptions } from 'react-query';
-import { DaoCreationSteps } from '@aragon/sdk-client';
 
 import { useAragon } from '../../context';
-import { NewDaoState, NewDaoParams, NewDaoStatus } from './types';
-
-const initialState: NewDaoState = {
-  daoAddress: null,
-  daoTxHash: null,
-  creationStatus: NewDaoStatus.IDLE,
-};
+import { NewDaoParams, NewDaoStatus } from './types';
 
 export function useNewDao(newDaoParams: NewDaoParams, options?: UseMutationOptions) {
+  // State
+  const [daoAddress, setDaoAddress] = useState<string | null>(null);
+  const [daoTxHash, setDaoTxHash] = useState<string | null>(null);
+  const [creationStatus, setCreationStatus] = useState<NewDaoStatus>(NewDaoStatus.IDLE);
   const { baseClient: client } = useAragon();
-  const [state, dispatch] = useReducer((state: NewDaoState, update: Partial<NewDaoState>) => {
-    return { ...state, ...update };
-  }, initialState);
 
+  // Async function to create dao
   const createDao = async () => {
-    dispatch({ creationStatus: NewDaoStatus.PINNING_METADATA });
-    const metadataUri = await client?.methods.pinMetadata(newDaoParams.daoMetadata)!;
+    try {
+      // Update the creation status and pin metadata
+      setCreationStatus(NewDaoStatus.PINNING_METADATA);
+      const metadataUri = await client?.methods.pinMetadata(newDaoParams.daoMetadata);
 
-    for await (const step of client?.methods?.createDao({ metadataUri, ...newDaoParams })!) {
-      try {
-        switch (step.key) {
-          case DaoCreationSteps.CREATING:
-            dispatch({
-              daoTxHash: step.txHash,
-              creationStatus: NewDaoStatus.CREATING_DAO,
-            });
-            break;
-          case DaoCreationSteps.DONE:
-            dispatch({ creationStatus: NewDaoStatus.SUCCESS, daoAddress: step.address });
-            break;
-        }
-      } catch (err) {
-        dispatch({ creationStatus: NewDaoStatus.ERROR });
-        console.error(err);
-      }
+      // Get the dao creation iterator
+      const steps = client?.methods.createDao({ metadataUri, ...newDaoParams });
+
+      // Set the txHash and update creation status
+      setDaoTxHash((await steps.next()).value.txHash);
+      setCreationStatus(NewDaoStatus.CREATING_DAO);
+
+      // Set the daoAddress and update creation status
+      setDaoAddress((await steps.next()).value.address);
+      setCreationStatus(NewDaoStatus.SUCCESS);
+    } catch (err) {
+      setCreationStatus(NewDaoStatus.ERROR);
+      console.error(err);
     }
   };
 
   return {
-    ...state,
+    // State variables
+    daoAddress,
+    daoTxHash,
+    creationStatus,
+    // pass the async function to useMutation and spread the mutation & options
     ...useMutation({
       mutationKey: ['newDao', newDaoParams],
       mutationFn: createDao,
