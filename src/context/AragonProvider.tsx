1628eb59b59720030fa4c8c3c8a8aae1ed397c3e
pythonpete32 <aaron@blackscale.io>
2023-03-10 21:42:05 +0400

    allow wagmi client to be passed in to Provider

--- src/context/AragonProvider.tsx
+++ src/context/AragonProvider.tsx
@@ -1,19 +1,25 @@
 import React, { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { WagmiConfig, createClient, configureChains, goerli } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
+import { Client, WagmiConfig } from 'wagmi';
+// import { publicProvider } from 'wagmi/providers/public';
 
 import { AragonSDKWrapper } from './AragonContext';
-const { provider, webSocketProvider } = configureChains([goerli], [publicProvider()]);
+// const { provider, webSocketProvider } = configureChains([goerli], [publicProvider()]);
 
-const client = createClient({
-  autoConnect: true,
-  provider,
-  webSocketProvider,
-});
+// const client = createClient({
+//   autoConnect: true,
+//   provider,
+//   webSocketProvider,
+// });
 
-export function AragonProvider({ children }: { children: ReactNode | false }): JSX.Element {
+export function AragonProvider({
+  client,
+  children,
+}: {
+  client: Client;
+  children: ReactNode | false;
+}): JSX.Element {
   return (
     <WagmiConfig client={client}>
       <AragonSDKWrapper>
