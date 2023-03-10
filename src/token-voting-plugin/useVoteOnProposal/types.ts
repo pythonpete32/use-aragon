bdb56d0fd7b03c5a631942f9681e2199fae82e14
pythonpete32 <aaron@blackscale.io>
2023-03-10 18:18:49 +0400

    rename enum

--- src/token-voting-plugin/useVoteOnProposal/types.ts
+++ src/token-voting-plugin/useVoteOnProposal/types.ts
@@ -1,6 +1,6 @@
 export enum VoteStatus {
   IDLE = 'idle',
-  WAITING_FOR_SIGNATURE = 'waitingForSignature',
+  WAITING_FOR_SIGNER = 'waitingForSigner',
   CONFIRMING_TRANSACTION = 'confirmingTransaction',
   SUCCESS = 'success',
   ERROR = 'error',
