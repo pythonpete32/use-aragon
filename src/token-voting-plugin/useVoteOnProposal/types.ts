d8032f9f07bf5039c1aef3d46be1b2e66c57d704
pythonpete32 <aaron@blackscale.io>
2023-03-10 17:57:56 +0400

    add `useVoteOnProposal`

---
+++ src/token-voting-plugin/useVoteOnProposal/types.ts
@@ -0,0 +1,7 @@
+export enum VoteStatus {
+  IDLE = 'idle',
+  WAITING_FOR_SIGNATURE = 'waitingForSignature',
+  CONFIRMING_TRANSACTION = 'confirmingTransaction',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
